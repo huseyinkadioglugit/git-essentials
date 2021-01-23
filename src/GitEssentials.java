f82b754a51640a964a38afe5943dcba344e7106d
Hüseyin Kadıoğlu <huseyin.kadioglu@linktera.com.tr>
2021-01-23 16:53:18 +0300

    before the merge

--- src/GitEssentials.java
+++ src/GitEssentials.java
@@ -2,5 +2,6 @@ public class GitEssentials {
   public static void main(String[] args) {
     System.out.println("Git essentials");
     System.out.println("This is another commit");
+    System.out.println("Before the merge");
   }
 }
